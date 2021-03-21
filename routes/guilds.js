3b8885b04e30ab39382d30e804e24471a6a1377c
Zerio <cnaucun2@gmail.com>
2021-03-21 15:12:16 +0100

    ⚡️ Optimization to get a guild

--- routes/guilds.js
+++ routes/guilds.js
@@ -4,12 +4,16 @@ module.exports.Router = class Routes extends Router {
     constructor() {
         super();
 
-        this.use('/', function (req, res) {
+        this.get('/', function (req, res) {
             if (!req.user) return res.redirect('/login');
 
-            if (!req.path.split('/')[1]) return res.send(req.user.guilds.map(x => x.name));
+            return res.send(req.user.guilds.map(x => x.name));
+        });
 
-            const guild = client.guilds.cache.get(req.path.split('/')[1]);
+        this.get('/:ID', function (req, res) {
+            if (!req.user) return res.redirect('/login');
+
+            const guild = client.guilds.cache.get(req.params.ID);
 
             if (!guild) return res.redirect('/invite');
             if (!guild.members.cache.get(req.user.me.id).hasPermission('MANAGE_GUILD')) return res.redirect('/guilds');
