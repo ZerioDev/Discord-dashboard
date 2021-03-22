eace7578fce4138c7f985b1c48342e4d4c943f29
SimonLeclere <simon-leclere@orange.fr>
2021-03-22 14:03:56 +0100

    ✨ Ping Command

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const client = new Client();
 client.config = require('./config');
 
 client.on('ready', () => client.dashboard = new Dashboard(client));
-
 client.on('newUser', (user) => console.log(`${user.username} just logged into the dashboard`));
+
+
+client.on('message', message => {
+	if(message.content.startsWith('!ping')) message.reply('Pong !');
+});
+
 
 client.login(client.config.client.token);
