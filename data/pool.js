562b2967569d6aa36ad2f513e625be0d13b4efcc
Zerio <cnaucun2@gmail.com>
2021-04-26 22:12:32 +0100

    🗃️ User cache

--- data/pool.js
+++ data/pool.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
+const { Collection } = require('discord.js');
 
 class Database {
     constructor(client) {
+        this.client = client;
+
         this.pool = new Pool(client.config.database);
 
         this.pool.on('error', (err) => console.error(err));
+
+        this.client.dbUsers = new Collection();
 
         console.log(`Successfully logged to the database as ${client.config.database.user}`);
     }
